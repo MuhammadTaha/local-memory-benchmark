8a12bac18a8a92d4941d07f9f2b4bf697539fb79
Muhammad Taha <muhammad.taha@sap.com>
2018-05-02 21:08:06 +0200

    Initial Commit

---
+++ InMemoryAssign1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  InMemoryAssign1
+//
+//  Created by Taha, Muhammad on 02.05.18.
+//  Copyright © 2018 Taha, Muhammad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
